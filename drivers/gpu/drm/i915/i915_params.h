c112e3b0de5264432b3ddfab441edafebf60c471
GreenDamTan <4781595+GreenDamTan@users.noreply.github.com>
2025-04-07 15:30:40 +0800

    change sriov max_vfs default to 7

--- drivers/gpu/drm/i915/i915_params.h
+++ drivers/gpu/drm/i915/i915_params.h
@@ -61,7 +61,7 @@ struct drm_printer;
 	param(unsigned int, request_timeout_ms, CONFIG_DRM_I915_REQUEST_TIMEOUT, CONFIG_DRM_I915_REQUEST_TIMEOUT ? 0600 : 0) \
 	param(unsigned int, lmem_size, 0, 0400) \
 	param(unsigned int, lmem_bar_size, 0, 0400) \
-	param(unsigned int, max_vfs, 0, 0400) \
+	param(unsigned int, max_vfs, 7, 0400) \
 	param(int, force_disable_ccs, 0, 0400) \
 	/* leave bools at the end to not create holes */ \
 	param(bool, enable_mtl_rcs_ccs_wa, true, 0x400) \
